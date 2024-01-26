f346c15167e63aa3e7d0694f40f46459b5a8f8a8
Pranav2456 <pranavvinodan@gmail.com>
2024-01-26 21:57:14 +0530

    First Commit

---
+++ src/Raffle.sol
@@ -0,0 +1,51 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+
+/**
+ * @title A sample Raffle contract
+ * @author Pranav Vinodan
+ * @notice Implements Chainlink VRFv2
+ */
+contract Raffle {
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRafflr() external payable{
+        require(msg.value >= i_entranceFee, "Not enough ETH to enter");
+    }
+
+    function pickWinner() public {}
+
+    /** Getter Function */
+
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
+}
