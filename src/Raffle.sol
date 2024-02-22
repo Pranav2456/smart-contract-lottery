98b33ac4c04a43fc0516406c0d635cff25ad1c10
Pranav2456 <pranavvinodan@gmail.com>
2024-02-22 20:18:03 +0530

    Implemented Chainlink Automation

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -32,16 +32,19 @@ import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2
  * @author Pranav Vinodan
  * @notice Implements Chainlink VRFv2
  */
+
+// CEI - Checks, Effects, Interactions - This is a pattern to follow when writing functions in Solidity. First, you check the inputs, then you execute the effects, and finally, you interact with other contracts.
 contract Raffle is VRFConsumerBaseV2 {
 
     error Raffle_NotEnoughEthSent();
     error Raffle_TransferFailed(); 
     error Raffle_RaffleNotOpen();
+    error Raffle_UpkeepNotNeeded(uint256 currentBalance, uint256 numPlayers, uint256 raffleState);
 
     enum RaffleState {
         OPEN,
         CALCULATING
-    }
+    } // Enums are used to create user-defined data types.
 
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 1;
@@ -60,6 +63,7 @@ contract Raffle is VRFConsumerBaseV2 {
 
     /** Events */
     event EnteredRaffle(address indexed player);
+    event PickedWinner(address indexed winner);
 
     constructor(uint256 entranceFee, uint256 interval, address vrfCoordinator, bytes32 gasLane, uint64 subscriptionId, uint32 callbackGasLimit) VRFConsumerBaseV2 (vrfCoordinator){
         i_entranceFee = entranceFee;
@@ -86,16 +90,40 @@ contract Raffle is VRFConsumerBaseV2 {
         emit EnteredRaffle(msg.sender);
     }
 
+    /**
+     * @dev This is the function that the Chainlink Keeper nodes call
+     * they look for `upkeepNeeded` to return True.
+     * the following should be true for this to return true:
+     * 1. The time interval has passed between raffle runs.
+     * 2. The lottery is open.
+     * 3. The contract has ETH.
+     * 4. Implicity, your subscription is funded with LINK.
+     */
+
+    function checkUpkeep(bytes memory /* checkData */) public view returns (bool upkeepNeeded, bytes memory /* performData */){
+         //check to see if enough time passed.
+        bool timeHasPassed = (block.timestamp - s_lastTimeStamp) >= i_interval; // block.timestamp is the current time in seconds since the epoch.
+        bool isOpen = RaffleState.OPEN == s_raffleState;
+        bool hasBalance = address(this).balance > 0;
+        bool hasPlayers = s_players.length > 0;
+        upkeepNeeded = (timeHasPassed && isOpen && hasBalance && hasPlayers);
+        return (upkeepNeeded, "0x0");
+    }
+
 //1. Get random number
 // 2. Use the random number to pick a player.
 // 3. Be automatically called.
-    function pickWinner() public {
-        //check to see if enough time passed.
-        if (block.timestamp - s_lastTimeStamp < i_interval){
-            revert();
-        } // block.timestamp is the current time in seconds since the epoch.
-
-        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+    function performUpkeep(bytes calldata /* performData */) external {
+        (bool upkeepNeeded,) = checkUpkeep("");
+        if(!upkeepNeeded){
+            revert Raffle_UpkeepNotNeeded(
+                address(this).balance,
+                s_players.length,
+                uint256(s_raffleState)
+            );
+        }
+        s_raffleState = RaffleState.CALCULATING;
+        i_vrfCoordinator.requestRandomWords(
             i_gasLane,
             i_subscriptionId,
             REQUEST_CONFIRMATIONS,
@@ -104,15 +132,22 @@ contract Raffle is VRFConsumerBaseV2 {
         );
     }
 
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+    function fulfillRandomWords(uint256 /*requestId*/, uint256[] memory randomWords) internal override {
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable winner = s_players[indexOfWinner];
         s_recentWinner = winner;
+        s_raffleState = RaffleState.OPEN;
+
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
+        emit PickedWinner(winner);
+
         (bool success,) = winner.call{value: address(this).balance}("");
         if(!success) {
             revert Raffle_TransferFailed();
 
         }  
+
     }
 
     /** Getter Function */
